4faa73c5bec869fd6ecbff7a508620d2acbdb9d2
Joao Pedro Poloni Ponce <jp052@hotmail.com>
2017-07-19 16:31:21 -0300

    Added animation

--- main.c
+++ main.c
@@ -9,11 +9,17 @@ static char far at LCD_INST inst;
 //static char far at DISPLAY_IR Display_dado ;
 //static char far at DISPLAY_IR Display_dado ;
 unsigned int Tm0 = 0;
+bit H = 0;//Flag (0)indica que o cursor deve se mover para baixo, (1) para cima
+bit L = 0;//Flag (0)indica que o cursor deve se mover para a direita, (1) para a esquerda
+unsigned int D = 0;
+unsigned int A = 0;
+unsigned char Ani;
 
-unsigned char lcdAddress[4][16] = {{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
-							{0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F},
-							{0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F},
-							{0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F}};
+unsigned char lcdAddress[4][16] = {
+{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
+{0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F},
+{0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F},
+{0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F}};
 
 void delay(unsigned int period){ 
 	int i = 0;
@@ -54,15 +60,20 @@ void lcdSetCursor(unsigned char row, unsigned char collumn) {
 //}
 
 void prog(){
-	IE = 0x82;// 10000010 Serial e timer 0
-  	TMOD = 0x01;// timer 0 habilitado
+	IE = 0x92;// 10000010 Serial e timer 0
+  	TMOD = 0x21;// timer 0 habilitado
+  	SCON = 0x50;// Serial
   	// Timers
 	TH0 = 0; // Timer 0 HIGH
 	TL0 = 0; // Timer 0 LOW
+	TH1 = 0;
 	TF0 = 0; // Flag de overflow do Timer 0
 	TR0 = 1; // Inicia (1) ou para (0) Timer 0
+	TF1 = 0; // Flag de overflow do Timer 0
+	TR1 = 1; // Inicia (1) ou para (0) Timer 0
 	TH0 = 0x4B;
 	TL0 = 0xFF;
+	TH1 = 0xFD;
 }
 
 /*void EscreveInst(){
@@ -78,14 +89,16 @@ void Display_Inicia(){
 */
 void main(void){
 	prog();
-	lcdCMD(0x38);
-	lcdCMD(0x06);
-	lcdCMD(0x0C);
+	lcdCMD(0x38);//Setup inicial
+	lcdCMD(0x06);//Setup inicial
+	lcdCMD(0x0C);//Setup inicial
 	
-	lcdHome(); 
-	lcdClear();
-	lcdSetCursor(2,6);
-	lcdWrite('A');
+	Ani = 'A';
+	/*
+	lcdHome();//Posiciona o cursor na posição [0][0]
+	lcdClear();//Limpa o conteudo da posição
+	lcdSetCursor(2,6);//Coloca o cursor na posição desejada
+	lcdWrite('A');//Escreve no cursor a letra desejada
 	delay(1000);
 	lcdSetCursor(3, 15);
 	lcdWrite('B');
@@ -96,9 +109,24 @@ void main(void){
 	lcdCMD(0x01);
 	lcdCMD(0x02);
 	lcdWrite('D');
-	delay(1000);
+	delay(1000);*/
 	
 	while(1){
+		lcdClear();
+		lcdSetCursor(A, D);
+		lcdWrite(Ani);
+		delay(500);
+		lcdClear();
+		if(!H) A++;
+		if(H) A--;
+		if(A == 3) H = !H;
+		if(A == 0) H = !H;
+		if(!L) D++;
+		if(L) D--;
+		if(D == 15) L = !L;
+		if(D == 0) L = !L;
+	
+	
 		if(Tm0 >= 20){
       			Tm0 = 0;
      	 		P1_0 = !P1_0;
@@ -110,4 +138,12 @@ void Timer_0() interrupt 1{
   	Tm0++;
   	TH0 = 0x4B;
 	TL0 = 0xFF;
+}
+
+void ChangeAni() interrupt 4{
+	RI = 0;//Preparado para mais um recebimento
+	Ani = SBUF;//Passa o dado recebido para a variavel
+	SBUF = 65;//ACK
+  	while(!TI);//Transmite ACK
+  	TI = 0;//Preparado para mais uma transmissao
 }
